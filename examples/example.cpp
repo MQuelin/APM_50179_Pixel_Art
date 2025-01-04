b162af86bc750b915b04bb7ccf2271c1c1182f6c
MQuelin <mq.no.spam@orange.fr>
2025-01-04 16:03:19 +0100

    First example with opencv
    Broken commit, can't assign value to opencv mat

--- examples/example.cpp
+++ examples/example.cpp
@@ -1,26 +1,88 @@
 #include <opencv2/core.hpp>
+#include <opencv2/core/hal/interface.h>
+#include <opencv2/core/mat.hpp>
+#include <opencv2/core/types.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgcodecs.hpp>
 
-#include <iostream>
+#include <xtensor/xadapt.hpp>
+#include <xtensor/xarray.hpp>
 
-using namespace cv;
+#include <filesystem>
+#include <iostream>
+#include <xtensor/xtensor_forward.hpp>
+
+// Utilities to convert back and forth between opencv's mat and xtensor's xarray
+// for use with depixel_lib
+xt::xarray<float> mat_to_arr(const cv::Mat &mat) {
+  int ndims = mat.dims;
+  assert(ndims == 2);
+  int nrows = mat.rows;
+  int ncols = mat.cols;
+  int nchannels = 3;
+  xt::xarray<float> arr = xt::empty<float>({nrows, ncols, nchannels});
+  for (int rr = 0; rr < nrows; rr++) {
+    for (int cc = 0; cc < ncols; cc++) {
+      cv::Vec3f pxl = mat.at<float>(rr, cc);
+      cv::Vec3f pxl_copy = pxl;
+      // std::cout << pxl[0] << std::endl;
+      for (int chan = 0; chan < nchannels; chan++) {
+        arr(rr, cc, chan) = pxl_copy[chan];
+      }
+    }
+  }
+  return arr;
+}
+
+cv::Mat arr_to_mat(const xt::xarray<float> &arr) {
+  int ndims = arr.dimension();
+  assert(ndims == 3 && "can only convert 3d xarrays");
+  int nrows = arr.shape()[0];
+  int ncols = arr.shape()[1];
+  cv::Mat mat(nrows, ncols, 3, CV_32FC3);
+  for (int rr = 0; rr < nrows; rr++) {
+    for (int cc = 0; cc < ncols; cc++) {
+      std::cout << arr(rr, cc, 0) << std::endl;
+      std::cout << arr(rr, cc, 1) << std::endl;
+      std::cout << arr(rr, cc, 2) << std::endl << std::endl;
+      for (int chan = 0; chan < 3; chan++) {
+        mat.at<cv::Vec3f>(rr, cc)[chan] = 1.; // arr(rr, cc, chan);
+      };
+    }
+  }
+  return mat;
+}
 
 int main() {
-  std::string image_path = samples::findFile("examples/TL_Creatures.png");
-  Mat img = imread(image_path, IMREAD_COLOR);
+  cv::Mat img_base =
+      imread("../examples/SpaceShip-24x24.png", cv::IMREAD_COLOR);
+  cv::Mat img;
+  img_base.convertTo(img, CV_32FC3, 1. / 255.);
 
   if (img.empty()) {
-    std::cout << "Could not read the image: " << image_path << std::endl;
+    std::cout << "Current directory: " << std::filesystem::current_path()
+              << std::endl;
+    std::cout << "Could not read the image: " << std::endl;
     return 1;
   }
 
   imshow("Display window", img);
-  int k = waitKey(0); // Wait for a keystroke in the window
+  cv::waitKey(0); // Wait for a keystroke in the window
 
-  if (k == 's') {
-    imwrite("starry_night.png", img);
-  }
+  int type = img.type();
+  std::cout << "image type: " << cv::typeToString(type) << std::endl;
+
+  std::cout << "mat_to_arr" << std::endl;
+  auto img_arr = mat_to_arr(img);
+  // std::cout << img_arr << std::endl;
+  std::cout << "arr_to_mat" << std::endl;
+  auto img2 = arr_to_mat(img_arr);
+  std::cout << img2 << std::endl;
+
+  cv::imwrite("../examples/SpaceShip-24x24-bis.png", img2);
+
+  imshow("Display window 2", img2);
+  cv::waitKey(0); // Wait for a keystroke in the window
 
   return 0;
 }
---
+++ src/depixelize.cpp
@@ -0,0 +1,38 @@
+#include "depixel_lib/depixelize.hpp"
+#include "depixel_lib/cells.hpp"
+#include "depixel_lib/graph.hpp"
+#include "depixel_lib/spline.hpp"
+
+namespace dpxl {
+
+xt::xarray<float> depixelize(xt::xarray<float> &img_array,
+                             float scaling_factor) {
+  // Processing steps:
+  // 1 - Establish similarity graph
+  // 2 - Resolve crossings
+  // 3 - Create reshaped cells
+  // 4 - Define splines based on reshaped cells
+  // 5 - Create new tensor with resolution scaled based on scaling
+
+  //// img_array : [r,c,3]
+  //// graph : [r,c,8] ? or object with the image array and another separate
+  //// array for neighbours ?
+  // xt::xarray graph = make_graph(img_array);
+  // resolve_crossings(graph);
+  //
+  //// cells : [4r, 4c, 3]
+  // xt::xarray cells = get_voronoi_cells(graph);
+  //
+  // Spline spline = spline_from_voronoi(cells);
+  //
+  // xt::xarray upscaled_img_arr =
+  // spline.render(std::floor(img_array.shape()[0]*scaling_factor),
+  // std::floor(img_array.shape()[1]*scaling_factor));
+  //
+  // return upscaled_img_arr;
+
+  // TODO actuallly process array correctly
+  return img_array;
+}
+
+} // namespace dpxl
