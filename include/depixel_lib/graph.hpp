443a8c1ef83af52597297aab36f258ac49236c16
Leandre Simon <leandre.simon@gmail.com>
2025-01-09 00:02:32 +0100

    Finalizing the similarity graph, adding a draw function to view them

--- include/depixel_lib/graph.hpp
+++ include/depixel_lib/graph.hpp
@@ -2,6 +2,7 @@
 #include <xtensor/xtensor_forward.hpp>
 #include <xtensor/xadapt.hpp>
 #include <xtensor/xarray.hpp>
+#include <opencv2/opencv.hpp>
 
 
 
@@ -15,15 +16,23 @@ public:
   xt::xarray<float> get_image();
   xt::xarray<bool> get_neighbours();
 
+  cv::Mat draw_neighbours();
+
 private:
   xt::xarray<float> m_img;
   xt::xarray<bool> m_neighbours;
 
   void compute_neighbours();
+  void remove_trivial_edges();
+
+  bool is_close_color(const xt::xarray<float>& color_pixel, const xt::xarray<float>& color_neighboor);
 
   // Define the thresholds for each channel (normalized to [0, 1] range)
   const float Y_THRESHOLD = 48.0 / 255.0;
   const float U_THRESHOLD = 7.0 / 255.0;
   const float V_THRESHOLD = 6.0 / 255.0;
+
+  // Scale factor for upscaling for the draw function
+    const int scale_factor = 100; // Adjust as needed for better visibility
 };
 } // namespace dpxl
